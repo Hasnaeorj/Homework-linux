fb40c8b48b5186075732dee9c0737a38456a6b1a
hasnaeorj <hasnaeourhejji2gmail.com>
2024-09-09 22:08:56 +0000

    age

---
+++ age.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+read -p "enter your year of birth " year
+echo your age is $((2024-year)) years old
+echo your age in 2020 was $((2020-year)) years old
+echo your age in 2030 will be $((2030-year)) years old
