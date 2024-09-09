bf06c31e2851f44f6175c901aab1aead81606ce3
hasnaeorj <hasnaeourhejji2gmail.com>
2024-09-09 22:05:07 +0000

    calculator

---
+++ calculator.sh
@@ -0,0 +1,7 @@
+read -p "enter 1st number " number1
+read -p "enter 2nd number " number2
+echo the sum is $((number1+number2))
+echo the difference is $((number1-number2))
+echo the product is $((number1*number2))
+echo the quotient is $((number1/number2))
+
