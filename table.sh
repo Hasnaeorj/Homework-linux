89cd5b46f34d5a9a5387e893cc78b059fb22974a
hasnaeorj <hasnaeourhejji2@gmail.com>
2024-09-09 22:35:51 +0000

    multiplication

---
+++ table.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for n in {1..10};
+do	
+echo "5 multiple by $n = $((5*n))"
+done
+echo
+for n in {1..10};
+do
+        echo 6 multiple by $n = $((6*$n))
+done
+echo 
+for n in {1..10};
+do
+        echo 7 multiple by $n = $((7*$n))
+done
+
